15f8bef40e02bee8f8148b2994e7b35c54d4dd14
globnot <antoineborda-pro@proton.me>
2026-01-22 11:43:49 +0100

    Create contain_start function

--- srcs/validation/validation.c
+++ srcs/validation/validation.c
@@ -6,7 +6,7 @@
 /*   By: aborda <aborda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 11:10:42 by aborda            #+#    #+#             */
-/*   Updated: 2026/01/22 11:37:09 by aborda           ###   ########.fr       */
+/*   Updated: 2026/01/22 11:42:35 by aborda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,34 @@ int	contain_exit(char **map)
 		while (map[i][j])
 		{
 			if (map[i][j] == 'E')
+			{
+				j++;
+				count++;
+			}
+			else
+				j++;
+		}
+		i++;
+	}
+	if (count == 1)
+		return (1);
+	return (0);
+}
+
+int	contain_start(char **map)
+{
+	int	i;
+	int	j;
+	int	count;
+
+	i = 0;
+	count = 0;
+	while (map[i] != NULL)
+	{
+		j = 0;
+		while (map[i][j])
+		{
+			if (map[i][j] == 'P')
 			{
 				j++;
 				count++;
