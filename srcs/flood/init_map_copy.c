16d101d243b600129a264d2ea53cac12e3d32456
globnot <antoineborda-pro@proton.me>
2026-01-29 14:32:34 +0100

    Fix bug in init_map_copy function

--- srcs/flood/init_map_copy.c
+++ srcs/flood/init_map_copy.c
@@ -6,7 +6,7 @@
 /*   By: aborda <aborda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/29 13:17:21 by aborda            #+#    #+#             */
-/*   Updated: 2026/01/29 14:20:34 by aborda           ###   ########.fr       */
+/*   Updated: 2026/01/29 14:31:39 by aborda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ char	**init_map_copy(t_map *map, t_flood *flood)
 {
 	int		i;
 
-	flood->map_array_copy = malloc(sizeof(char *) * map->nb_line + 1);
+	flood->map_array_copy = malloc(sizeof(char *) * (map->nb_line + 1));
 	if (flood->map_array_copy == NULL)
 		return (free(flood), NULL);
 	i = 0;
@@ -27,5 +27,6 @@ char	**init_map_copy(t_map *map, t_flood *flood)
 			return (free_flood(flood), NULL);
 		i++;
 	}
+	flood->map_array_copy[i] = NULL;
 	return (flood->map_array_copy);
 }
