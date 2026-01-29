fd5a7218ba39677165eed38ddb093738dbcce192
globnot <antoineborda-pro@proton.me>
2026-01-29 16:15:08 +0100

    Remove test from main

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: aborda <aborda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/15 14:29:22 by aborda            #+#    #+#             */
-/*   Updated: 2026/01/29 16:12:36 by aborda           ###   ########.fr       */
+/*   Updated: 2026/01/29 16:14:52 by aborda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,6 @@ int	main(int ac, char **av)
 		flood = init_s_flood(map);
 		if (flood == NULL)
 			return (err_msg(ERR_INIT_S_FLOOD));
-		ft_printf("COLLECTIBLES = %d\n", flood->nb_collectible);
 		free_map(map);
 		free_flood(flood);
 		return (err_msg(ERR_OK));
