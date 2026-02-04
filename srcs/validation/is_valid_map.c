4c830a1fb5913beb7f3e4f5f4e672e41590fa053
globnot <antoineborda-pro@proton.me>
2026-02-04 08:43:13 +0100

    Fix memory bug

--- srcs/validation/is_valid_map.c
+++ srcs/validation/is_valid_map.c
@@ -6,7 +6,7 @@
 /*   By: aborda <aborda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 11:10:42 by aborda            #+#    #+#             */
-/*   Updated: 2026/01/31 09:36:02 by aborda           ###   ########.fr       */
+/*   Updated: 2026/02/04 08:39:04 by aborda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 int	is_valid_map(t_map *map)
 {
+	if (!is_rectangular(map))
+		return (err_msg(ERR_MAP_FORMAT));
 	if (!check_valid_chars(map))
 		return (err_msg(ERR_MAP_ELEMENTS));
 	if (!contain_element(map, 'E'))
@@ -22,8 +24,6 @@ int	is_valid_map(t_map *map)
 		return (err_msg(ERR_MAP_ELEMENTS));
 	if (!contain_element(map, 'C'))
 		return (err_msg(ERR_MAP_ELEMENTS));
-	if (!is_rectangular(map))
-		return (err_msg(ERR_MAP_FORMAT));
 	if (!is_closed_by_walls(map))
 		return (err_msg(ERR_MAP_FORMAT));
 	if (!check_path(map))
